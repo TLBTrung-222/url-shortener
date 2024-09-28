53d4d3d2c9a12b39849c365e5368ae755ff44110
TrungTLB <baotrung2852003@gmail.com>
2024-09-28 17:09:12 +0700

    add validation rule for password

--- server/src/utils/validationRules.ts
+++ server/src/utils/validationRules.ts
@@ -5,7 +5,13 @@ export const urlValidationRules = [
         .isURL()
         .withMessage('Invalid URL format')
         .notEmpty()
-        .withMessage('URL is required')
+        .withMessage('URL is required'),
+    body('password')
+        .optional()
+        .isString()
+        .withMessage('Password must be a string')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long')
 ]
 
 // export const urlCodeValidationRules = [
